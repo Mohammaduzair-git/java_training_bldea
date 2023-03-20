b033a182685fc859de05d0f2645758c1d86ad134
Jubair Aurangabadi <Jubair Aurangabadi@HP-LAPTOP>
2023-03-20 13:07:27 +0530

    first commit

---
+++ src/main/java/java_training_bldea/test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package java_training_bldea;
+
+/**
+ * @author Jubair Aurangabadi
+ *
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
