3405e93cce4b5b2acac7ff670233f689496d9b7a
Admin <Admin@DESKTOP-IBP9R3R>
2023-03-20 13:28:06 +0530

    first commit

--- src/main/java/java_training_bldea/test1.java
+++ src/main/java/java_training_bldea/test1.java
@@ -1,20 +1,18 @@
-/**
- * 
- */
-package java_training_bldea;
-
-/**
- * @author Jubair Aurangabadi
- *
- */
+import java.util.Scanner;
 public class test1 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter age");
+		int age=sc.nextInt();
 
+		if(age<18) {
+			System.out.println("u r still minor");
+		}
+		else if(age>65) {
+			System.out.println("u r senior");
+		}
+		else {
+			System.out.println("party");
+		}
+	}
 }
